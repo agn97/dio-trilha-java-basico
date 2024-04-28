56f0910d37ac970edfed5882993f8302054fa557
agn97 <alangomess1997@gmail.com>
2024-04-28 12:20:50 -0300

    concatenacoes

---
+++ tipos-variaveis/src/operadores.java
@@ -0,0 +1,65 @@
+import java.sql.Date;
+
+public class operadores {
+    public static void main(String[] args) {
+        String nome = "Alan";
+        int idade = 27;
+        double peso = 82.4;
+        char sexo = 'M';
+        boolean doadorOrgao = true;
+        Date dataNascimento = new Date(1997-04-22);
+
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(peso);
+        System.out.println(sexo);
+        System.out.println(doadorOrgao);
+        System.out.println(dataNascimento);
+
+        double soma = 105 + 17.5;
+        int subtracao = 113 - 25;
+        int multiplicacao = 20 * 7;
+        int divisao = 15 / 3;
+        int modulo = 18 % 3;
+        double resultado = (10*7) + (20/4);
+
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(multiplicacao);
+        System.out.println(divisao);
+        System.out.println(modulo);
+        System.out.println(resultado);
+
+
+        String nomeCompleto = "Linguagem" + "Java";
+            System.out.println(nomeCompleto);
+
+
+        String concatenacao = "?";
+        System.out.println(concatenacao);
+
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);
+
+
+
+
+
+
+
+    }
+    
+}
