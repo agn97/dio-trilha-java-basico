61ca837fed3a25739883e18f4eea6f26f4c934d4
agn97 <alangomess1997@gmail.com>
2024-04-28 23:35:23 -0300

    operador para definir uma condicao dentre dois valores

---
+++ tipos-variaveis/src/ternario.java
@@ -0,0 +1,25 @@
+public class ternario {
+        public static void main (String [] args) {
+           int a,b;
+            a = 6;
+            b = 6;
+    
+           String resultado =  a==b ? "verdadeiro" : "falso" ;
+    
+          // if(a==b)
+          //      resultado = "verdairo";
+         //   else
+          //      resultado = "falso";
+    
+            System.out.println(resultado);
+            
+    
+    
+    
+    
+        }
+        
+    
+    
+    
+}
