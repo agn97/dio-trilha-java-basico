09f677c3c1f6e38eeafa1d0680a11cc78968437f
agn97 <alangomess1997@gmail.com>
2024-04-29 00:19:43 -0300

    relacao entre duas variaveis ou expressoes

--- operadores-relacionais/operadores-relacionais/src/operadoreRelacionais.java
+++ operadores-relacionais/operadores-relacionais/src/operadoreRelacionais.java
@@ -1,4 +1,47 @@
 public class operadoreRelacionais {
-    
+    public static void main(String[] args) {
+        String nomeUm = "Alan";
+        String nomeDois = new String("Alan");
+
+        System.out.println(nomeUm == nomeDois);
+        System.out.println(nomeUm.equals(nomeDois));
+
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1==numero2;
+
+        if (numero1 == numero2){ //estrutura condicional if e else
+           System.out.println("a nossa condicao e verdadeira"); 
+        }
+        else
+        System.out.println("nossa condicao e falsa");// fim da estrutura
+
+        System.out.println("numeroUm é igual a numeroDois? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("numeroUm é diferente a numeroDois? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numeroUm é maior a numeroDois? " + simNao);
+
+        simNao = numero1 >= numero2;
+
+        System.out.println("numeroUm é maior ou igual a numeroDois? " + simNao);
+
+        simNao = numero1 < numero2;
+
+        System.out.println("numeroUm é menor a numeroDois? " + simNao);
+
+        simNao = numero1 <= numero2;
+
+        System.out.println("numeroUm é menor ou igual a numeroDois? " + simNao);
+        
+
+
+    }
     
 }
