b030a8e3d82d30bca01627040166513903cef01c
agn97 <alangomess1997@gmail.com>
2024-04-29 00:49:22 -0300

    permite criar expressoes logicas a partir da juncao de duas ou mais expressoes

---
+++ operadores-logicos/operadores-logicos/src/operadoresLogicos.java
@@ -0,0 +1,21 @@
+public class operadoresLogicos {
+    public static void main (String [] args) {
+        boolean condicao1 = true;
+        boolean condicao2 = true;
+
+    if (condicao1 && (7 > 4)){
+        System.out.println(" as duas sao verdadeiras");
+
+    }
+
+    if (condicao1 || condicao2){
+        System.out.println(" UMA DAS DUAS sao verdadeiras");
+
+   
+
+}
+    System.out.println("FIM");
+
+}
+
+}
